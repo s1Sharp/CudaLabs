02e1abc9c7e7b5457f2f16f016f8c8d76649d320
Maksim "s1Sharp" Tsarkov <s1sharp.it@gmail.com>
2021-09-05 23:21:42 +0300

    .gitingore upd

--- lab_1/kernel.cu
+++ lab_1/kernel.cu
@@ -1,7 +1,9 @@
 ﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
-#include <ostream>
+#include <iostream>
+#include <thread>
+#include <functional>
 
 void print_info(FILE *);
 
@@ -10,9 +12,11 @@ __global__ void add(int* a, int* b, int* c)
     *c = *a + *b;
 }
 
+FILE* file = fopen("output.txt", "w");
+
 int main() 
 {
-    FILE* file = fopen("output.txt", "w");
+    
 
     int a, b, c;
     // host copies of variables a, b & c
@@ -26,7 +30,7 @@ int main()
     // Setup input values  
     c = int();
     a = 7777u;
-    b = 3333u;
+    b = 333u;
     // Copy inputs to device
     cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
@@ -39,39 +43,11 @@ int main()
         fprintf(file,"CUDA error copying to Host: %s\n", cudaGetErrorString(err));
     }
     fprintf(file,"result sum of %d and %d is %d\n",a , b, c);
-    print_info(file);
-    fclose(file);
+
+   
     // Cleanup
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
     return 0;
-}
-
-void print_info(FILE * f)
-{
-    int            deviceCount;
-    cudaDeviceProp devProp;
-
-    cudaGetDeviceCount(&deviceCount);
-
-    fprintf(f,"Found %d devices\n", deviceCount);
-
-    for (int device = 0; device < deviceCount; device++)
-    {
-        cudaGetDeviceProperties(&devProp, device);
-
-        fprintf(f,"Device %d\n", device);
-        fprintf(f,"Compute capability     : %d.%d\n", devProp.major, devProp.minor);
-        fprintf(f,"Name                   : %s\n", devProp.name);
-        fprintf(f,"Total Global Memory    : %llu\n", devProp.totalGlobalMem);
-        fprintf(f,"Shared memory per block: %d\n", devProp.sharedMemPerBlock);
-        fprintf(f,"Registers per block    : %d\n", devProp.regsPerBlock);
-        fprintf(f,"Warp size              : %d\n", devProp.warpSize);
-        fprintf(f,"Max threads per block  : %d\n", devProp.maxThreadsPerBlock);
-        fprintf(f,"Total constant memory  : %d\n", devProp.totalConstMem);
-        fprintf(f,"Total mmultiProcessor Count  : %d\n", devProp.multiProcessorCount);
-    };
-
-    return;
 }
