318d6a2a6b28712e3b66b915c8da4e23a8ed020c
Maksim "s1Sharp" Tsarkov <s1sharp.it@gmail.com>
2021-11-12 19:01:06 +0300

    add pi without atomic add by threadIdx

---
+++ CudaLabs/lab_PiByThreadidx/kernel.cu
@@ -0,0 +1,42 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cmath>
+#include <stdio.h>
+
+#define N 1000
+
+
+__device__ bool Check(const float& x, const float& y)
+{
+    return x * x + y * y <= 1;
+}
+
+__global__ void piCalc(float* count) {
+
+    float x = float(blockIdx.x) / N;
+    float y = float(threadIdx.x) / N;
+    Check(x, y) ? count[threadIdx.x * N + blockIdx.x] = 1 : count[threadIdx.x * N + blockIdx.x] = 0;
+}
+
+int main()
+{
+    float* sum = new float[N * N];
+    float* count;
+
+    cudaMalloc((void**)&count, N * N * sizeof(float));
+
+    piCalc << < N, N >> > (count);
+
+    cudaMemcpy(sum, count, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+
+    float ans = 0;
+    for (int i = 0; i < N * N; ++i) {
+        ans += sum[i];
+    }
+    printf("Pi is %f\n", ans * 4 / N / N);
+
+    delete [] sum;
+    cudaFree(count);
+    return 0;
+}
