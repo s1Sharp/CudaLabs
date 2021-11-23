f9a3e86ce31ff2ca900181ef0183c12fe22cff44
Maksim "s1Sharp" Tsarkov <s1sharp.it@gmail.com>
2021-11-23 23:15:43 +0300

    add comment

--- CudaLabs/homework_7/kernel.cu
+++ CudaLabs/homework_7/kernel.cu
@@ -7,16 +7,22 @@
 #include "rectangle.h"
 
 const size_t vectorSize = 100;
+const size_t vectorSize2 = 3;
+const size_t vectorSize3 = 333;
 
 int main()
 {
     meraVector(vectorSize);
+    meraVector(vectorSize2);
+    meraVector(vectorSize3);
+    /*
     linspace(0.f, 1.f, 5);
     linspace(0.f, 1.f, 5, false);
 
     integralSimpson(1.f, 2.f, 512);
 
     integralRectangle(1.f, 2.f, 512);
+    */
 
 
     return 0;
--- CudaLabs/homework_7/meraVector.h
+++ CudaLabs/homework_7/meraVector.h
@@ -10,13 +10,20 @@
 template <typename T>
 __global__ void kernel(T* ptr, size_t size)
 {
+    //cache for reduction size N * sizeof(float) bytes
     extern __shared__ T cache[];
+
+    //index for vector valie
     size_t tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    //index for reduction inside block 
     size_t cacheIndex = threadIdx.x;
 
     cache[cacheIndex] = ptr[tid] * ptr[tid];
 
+    //sync before reduction
     __syncthreads();
+
     // reduction
     size_t i = blockDim.x / 2;
 
@@ -28,22 +35,25 @@ __global__ void kernel(T* ptr, size_t size)
         __syncthreads();
         i /= 2;
     }
+    //save result reduction in ptr[index of block]
     ptr[blockIdx.x] = cache[0];
     return;
 }
 
+//generate random vector
 template <typename T>
 void generateVector(T* _ptr, size_t size)
 {
     srand(4721u);
     for (size_t i = 0; i < size; i++)
     {
-        _ptr[i] = static_cast<T>(rand() % 32);
+        _ptr[i] = static_cast<T>(rand() % 999999);
     }
     return;
 
 }
 
+//func caculate evclid norma on CPU
 template <typename T>
 T checkResult(T* _ptr, size_t size)
 {
@@ -60,22 +70,39 @@ T checkResult(T* _ptr, size_t size)
 
 void meraVector(size_t N)
 {
+    //generate vector size N
     float* ptr = new float[N];
     generateVector(ptr, N);
+
+    printf("\n");
+    for (int i = 0; i < N; i++)
+    {
+        printf("%f ", ptr[i]);
+    }
+    printf("\n");
+
+    //calculate result on CPU
     float checkResultCPU = checkResult(ptr, N);
 
+    //cpy vector to device memory
     float* dev_ptr;
     cudaMalloc((void**)&dev_ptr, sizeof(float) * N);
     cudaMemcpy(dev_ptr, ptr, sizeof(float) * N, cudaMemcpyHostToDevice);
 
+
+    //define threads and block dimension
     size_t THREADS_PER_BLOCK = 64;
     size_t BLOCKS_PER_GRID =
         (N / THREADS_PER_BLOCK) + 1;
 
+    //start GPU kernel
     kernel << <BLOCKS_PER_GRID, THREADS_PER_BLOCK, THREADS_PER_BLOCK * sizeof(float) >> > (dev_ptr, N);
 
+    //cpy result to host memory
     cudaMemcpy(ptr, dev_ptr, sizeof(float) * N, cudaMemcpyDeviceToHost);
 
+
+    //calculate reduction result on cpu
     float result = 0.f;
     for (size_t i = 0; i < BLOCKS_PER_GRID; i++)
     {
